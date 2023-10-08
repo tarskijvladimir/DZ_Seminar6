fdc221e639fc4898586bdf35206f175a11f57ee3
vladimir <tarskij-vladimir@yandex.ru>
2023-10-09 00:24:07 +0300

    added41

---
+++ Задача 41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M  чисел. Посчитайте, сколько чисел больше 0 ввел пользователь
+
+
+
+       int count = 0;
+        Console.WriteLine("Введите числа через пробел:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+    
+        for (int i = 0; i < numbers.Length; i++)
+         {
+            if (int.TryParse(numbers[i], out int n) && n > 0)
+         {
+            count++;
+         }}
+
+    //     foreach (string number in numbers) 
+    //     {
+    //         if (int.TryParse(number, out int n) && n > 0)
+    //         {
+    //             count++;
+    //         }
+    //    }
+        Console.WriteLine($"Количество чисел больше нуля: {count}");
+    
+
